c4ef441810c357640b6e763c8afbd6960883f00e
The Buildbot <buildbot@angular2buch.de>
2018-12-14 19:07:20 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/8134c19b08d91f76be8b43bec7fd9f3d2d624c27
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/461

--- src/app/book-monkey/iteration-7/i18n/search/search.component.spec.ts
+++ src/app/book-monkey/iteration-7/i18n/search/search.component.spec.ts
@@ -1,12 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable, of } from 'rxjs';
 
 import { SearchComponent } from './search.component';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let bs: BookStoreService;
+
+  const bookDb: Book[] = [
+    {
+      isbn: '111',
+      title: 'Book 1',
+      subtitle: 'Subtitle Book 1',
+      authors: ['author1'],
+      published: new Date()
+    },
+    {
+      isbn: '112',
+      title: 'Book 2',
+      authors: ['author1', 'author2'],
+      published: new Date(2015, 1, 1)
+    }
+  ];
+
+  let bookStoreServiceStub: Partial<BookStoreService>;
+  bookStoreServiceStub = {
+    getAllSearch: (searchTerm: string): Observable<Book[]> => {
+      return of(
+        bookDb.filter(item =>
+          Object.keys(item).some(k => item[k] != null && item[k].toString().includes(searchTerm))
+        )
+      );
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,6 +45,9 @@ describe('SearchComponent', () => {
       schemas: [ NO_ERRORS_SCHEMA ],
       imports: [
         HttpClientTestingModule
+      ],
+      providers: [
+        { provide: BookStoreService, useValue: bookStoreServiceStub }
       ]
     })
     .compileComponents();
@@ -23,9 +57,33 @@ describe('SearchComponent', () => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    bs = TestBed.get(BookStoreService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should return objects containing the given search term', () => {
+    bs.getAllSearch('111')
+      .subscribe(res => {
+        expect(res).toEqual([bookDb[0]]);
+      });
+    bs.getAllSearch('or2')
+      .subscribe(res => {
+        expect(res).toEqual([bookDb[1]]);
+      });
+    bs.getAllSearch('Sub')
+      .subscribe(res => {
+        expect(res).toEqual([bookDb[0]]);
+      });
+    bs.getAllSearch('11')
+      .subscribe(res => {
+        expect(res).toEqual([...bookDb]);
+      });
+    bs.getAllSearch('abcdefg')
+      .subscribe(res => {
+        expect(res).toEqual([]);
+      });
+    bs.getAllSearch('2015')
+      .subscribe(res => {
+        expect(res).toEqual([bookDb[1]]);
+      });
   });
 });
