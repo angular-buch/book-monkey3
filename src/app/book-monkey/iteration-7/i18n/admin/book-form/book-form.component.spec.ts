fceb8d1a200bfc56f8d3e2e20a124f7be62b113c
The Buildbot <buildbot@angular2buch.de>
2018-12-14 22:20:27 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/1b890b6c33de1fe45cd26371f3454a7ead64f768
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/618

--- src/app/book-monkey/iteration-7/i18n/admin/book-form/book-form.component.spec.ts
+++ src/app/book-monkey/iteration-7/i18n/admin/book-form/book-form.component.spec.ts
@@ -1,8 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { BookFormComponent } from './book-form.component';
 import { Book } from '../../shared/book';
@@ -10,12 +11,13 @@ import { Book } from '../../shared/book';
 describe('BookFormComponent', () => {
   let component: BookFormComponent;
   let fixture: ComponentFixture<BookFormComponent>;
+  let submitEl: DebugElement;
 
   const expectedBook: Book = {
     isbn: '1234567890',
     title: 'Test',
     authors: ['Author 1'],
-    published: new Date(),
+    published: new Date(100000000000),
     subtitle: 'something',
     thumbnails: [{
       title: 'my title',
@@ -54,6 +56,7 @@ describe('BookFormComponent', () => {
     fixture = TestBed.createComponent(BookFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    submitEl = fixture.debugElement.query(By.css('button[type=submit]'));
   });
 
   it('should initialize the form', () => {
@@ -90,4 +93,40 @@ describe('BookFormComponent', () => {
     });
   });
 
+  it('should emit the FormData', () => {
+    // spy on submit event
+    spyOn(component.submitBook, 'emit');
+
+    // check if empty elements will be filtered out
+    component.book = {
+      ...expectedBook,
+      authors: [ ...expectedBook.authors, '', null ],
+      thumbnails: [ ...expectedBook.thumbnails,
+        { url: '', title: 'should be filtered out' },
+        { url: '', title: null },
+        { url: null, title: '' }
+      ]
+    };
+    component.ngOnChanges();
+    component.submitForm();
+
+    // check emitted event data, expect filtered data
+    expect(component.submitBook.emit).toHaveBeenCalledWith(expectedBook);
+
+    // check if form has been resetted
+    expect(component.bookForm.value).toEqual({
+      isbn: null,
+      title: null,
+      authors: [ null, null, null ],
+      published: null,
+      subtitle: null,
+      thumbnails: [
+        { url: null, title: null },
+        { url: null, title: null },
+        { url: null, title: null },
+        { url: null, title: null }
+      ],
+      description: null
+    });
+  });
 });
