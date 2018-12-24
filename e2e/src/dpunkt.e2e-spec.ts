59c36035292833dda6432dd4e71f2b11d26249ea
The Buildbot <buildbot@angular2buch.de>
2018-12-24 13:36:07 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/01b87c2be80900f0192016c01466b31f439a07dd
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/1494

---
+++ e2e/src/dpunkt.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { /*BS*/browser, element, by/*BE*/ } from 'protractor';
+
+describe('dpunkt.verlag', () => {
+
+  // does not wait for on angular on a non-angular page
+  beforeAll(() => /*BS*/browser.ignoreSynchronization = true/*BE*/);
+
+  it('should just call it Angular', () => {
+    /*BS*/browser/*BE*/.get('http://dpunkt.de/buecher/12400/9783864903571-angular.html');
+    const heading = /*BS*/element/*BE*/(/*BS*/by/*BE*/.tagName('h1'));
+    expect(heading.getText()).toEqual('Angular');
+  });
+
+  afterAll(() => /*BS*/browser.ignoreSynchronization = false/*BE*/);
+});
+
