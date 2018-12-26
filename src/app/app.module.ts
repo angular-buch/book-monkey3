3780dcd76213b166b7f1e586cbf64ed9ef73e3bf
The Buildbot <buildbot@angular2buch.de>
2018-12-26 12:00:58 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/3d83e42b15fe5bbf790d8ccaa7a0c121f9845728
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/1589

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; // NEW
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
