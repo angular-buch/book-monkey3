c56faa5dd114bab6287bb35d6ddf15dd4d74aedf
The Buildbot <buildbot@angular2buch.de>
2019-01-20 11:43:58 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/265e2d8e3253d834dd94eedd0841dc3017fb8e6e
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/2302

--- src/app/book-monkey/iteration-3/rxjs/shared/book-raw.ts
+++ src/app/book-monkey/iteration-3/rxjs/shared/book-raw.ts
@@ -2,10 +2,10 @@ export interface BookRaw {
   isbn: string;
   title: string;
   authors: string[];
-  published: string;
+  published: string; 
   subtitle?: string;
   rating?: number;
-  thumbnails?: ThumbnailRaw[];
+  thumbnails?: ThumbnailRaw[]; 
   description?: string;
 }
 
